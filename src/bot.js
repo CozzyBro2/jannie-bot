727bb7941b593c133df58e53059389a70d6ae41f
CozzyBro2 <gojinhan2@gmail.com>
2023-02-24 22:48:14 -0500

    intents

--- src/bot.js
+++ src/bot.js
@@ -2,16 +2,24 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 const token = process.env['BOT_TOKEN']
 
-if (token) {
-    const client = new Client ({
-        intents: [GatewayIntentBits.Guilds]
-    });
-
-    client.once(Events.ClientReady, botClient => {
-        console.log(`Logged in as ${botClient.user.tag}`)
-    })
-
-    client.login(token)
-} else {
+if (!token) {
     console.error('Missing BOT_TOKEN environment variable.')
+    return
 }
+
+const client = new Client ({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once(Events.ClientReady, botClient => {
+    console.log(`Logged in as ${botClient.user.tag}`)
+})
+
+client.on(Events.MessageCreate, message => {
+    console.log(message.content)
+})
+
+client.login(token)
