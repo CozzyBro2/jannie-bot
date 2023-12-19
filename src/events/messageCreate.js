ec83bac5069620b1e380f52426c582da3d432ef5
CozzyBro2 <gojinhan2@gmail.com>
2023-12-18 19:44:30 -0500

    cheeky debug command and network optimization

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -7,7 +7,7 @@ const model = genAI.getGenerativeModel({model: "gemini-pro"})
 const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
 
 const generationConfig = {
-    temperature: 1,
+    temperature: 0,
     topK: 1,
     topP: 1,
     maxOutputTokens: 1024,
@@ -48,8 +48,15 @@ module.exports = {
             const mentioned = message.mentions.users.first()
 
             if (mentioned && mentioned.id === message.client.user.id) {
-                const content = message.content.replace(/<@!?\d+>/g, '');
-                
+                const content = `(${message.author.username}) says:` + message.content.replace(/<@!?\d+>/g, '');
+
+                if (message.author.id === process.env.OWNER_ID && content.includes("dump your history")) {
+                    let historyStr = ""
+                    history.map(info => historyStr += `\n \*\*${info.role}\*\*: \`${info.parts}\``)
+                    await message.reply(trim(historyStr, 1024))
+                    return
+                }
+
                 try {
                     const result = await chat.sendMessageStream(prompt + content)
 
@@ -62,11 +69,12 @@ module.exports = {
     
                         if (!msg) {
                             msg = await message.reply({content: chunk.text(), fetchReply: true})
+                            return
                         }
-    
+
                         await msg.edit(text)
                     }
-    
+
                     history.push({role: "user", parts: content})
                     history.push({role: "model", parts: text})
                 } catch (err) {
